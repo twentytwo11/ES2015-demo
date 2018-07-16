3c3a91b957a00fbfc1133ec46a98a35b7ac8363c
张安伦 <anlunz@126.com>
2018-07-16 12:12:12 +0800

    promise-demo2

--- chapter3/promise-demo2.js
+++ chapter3/promise-demo2.js
@@ -6,3 +6,12 @@ let promise = new Promise((resolve,reject) => {
     .catch((err) => {
         console.log(err)
     })
+
+// 穿透
+let promise2 = new Promise((resolve,reject) => {
+    resolve(8)
+})
+.then(1)
+.catch(null)//应保证then 和 catch 接受的始终为函数参数
+.then(Promise.resolve(9))
+.then(res => console.log(res))
